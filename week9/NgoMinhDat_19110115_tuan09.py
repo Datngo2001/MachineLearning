8c0a8d5e505781a89131d92fdff03b62d7e42110
DatNgo <ngominhdat110115@gmail.com>
2022-05-11 13:23:33 +0700

    week 9

--- week9/NgoMinhDat_19110115_tuan09.py
+++ week9/NgoMinhDat_19110115_tuan09.py
@@ -740,9 +740,44 @@ if let_plot:
 
 DONE = 1 
 
+########################
+# Lasso regularization #
+########################
+from sklearn.linear_model import Lasso, ElasticNet, LinearRegression
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import PolynomialFeatures, StandardScaler
 
+m = 20
+np.random.seed(15);
+X = 3*np.random.rand(m, 1)
+y_no_noise = 1 + 0.5*X 
+y = y_no_noise + np.random.randn(m, 1)/1.5
+X_test = np.linspace(0, 3, 100).reshape(100, 1)
 
+lasso_reg = Lasso(alpha=1, random_state=42)
+lasso_reg.fit(X, y)
 
+plt.subplot(122)
+plot_model(Lasso, polynomial=True, alphas=(0, 10**-7, 1), random_state=42)
+plt.title("Train polynomial models (degree = 10)", fontsize=font_size)
+plt.show()
 
- 
+###############
+# Elastic net #
+###############
+r_list = (0,0.5,1)
 
+plt.plot(X, y, "k.")
+plt.plot(X, y_no_noise, 'k-', linewidth=3, label="true model")
+for r, plot_style in zip(r_list, ("g-", "b-", "r-")):
+    model = ElasticNet(l1_ratio = r, alpha = 10**-7)
+    model = Pipeline([
+        ("poly_features", PolynomialFeatures(degree=10, include_bias=False)),
+        ("std_scaler", StandardScaler()),
+        ("regul_reg", model)   ])
+    model.fit(X, y)
+    y_test_regul = model.predict(X_test)
+    plt.plot(X_test, y_test_regul, plot_style, linewidth=2, label=r"$r = $" + str(r))
+plt.legend(loc="upper left", fontsize=font_size-1)
+plt.xlabel("$x_1$", fontsize=font_size)
+plt.axis([0, 3, 0, 4])
